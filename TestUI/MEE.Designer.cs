19cf84c01be28100a086b1ca3cddd37ac5504a7c
Huntereb <eastonb97@yahoo.com>
2014-12-13 20:09:36 -0500

    First Commit

---
+++ TestUI/MEE.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace TestUI
+{
+    partial class MEE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(9, 81);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(36, 20);
+            this.numericUpDown1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Item";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Form";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(75, 109);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Evolution 1";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(9, 36);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(50, 20);
+            this.numericUpDown2.TabIndex = 11;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.numericUpDown3);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.numericUpDown4);
+            this.groupBox2.Location = new System.Drawing.Point(93, 91);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(75, 109);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Evolution 2";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(9, 36);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(50, 20);
+            this.numericUpDown3.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Form";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Item";
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(9, 81);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(36, 20);
+            this.numericUpDown4.TabIndex = 8;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 68);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(65, 17);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Enabled";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(93, 68);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(65, 17);
+            this.checkBox2.TabIndex = 14;
+            this.checkBox2.Text = "Enabled";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(156, 21);
+            this.comboBox1.TabIndex = 15;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // MEE
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(180, 212);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MEE";
+            this.Text = "Mega Evo Editor";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ TestUI/MEE.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.IO;
+
+namespace TestUI
+{
+    public partial class MEE : Form
+    {
+
+        private string pathsource;
+        private string basePath;
+        private string[] files;
+        private List<string> monNames;
+
+        public MEE()            //All the initial settings
+        {
+            InitializeComponent();
+            numericUpDown1.Maximum = 10;
+            numericUpDown1.Minimum = 0;
+            numericUpDown2.Maximum = 775;
+            numericUpDown2.Minimum = 0;
+            numericUpDown3.Maximum = 775;
+            numericUpDown3.Minimum = 0;
+            numericUpDown4.Maximum = 10;
+            numericUpDown4.Minimum = 0;
+
+            button2.Enabled = false;
+            comboBox1.Enabled = false;
+            checkBox1.Enabled = false;
+            checkBox1.Checked = false;
+            checkBox2.Enabled = false;
+            checkBox2.Checked = false;
+            groupBox1.Enabled = false;
+            label2.Enabled = false;
+            numericUpDown1.Enabled = false;
+            label3.Enabled = false;
+            numericUpDown2.Enabled = false;
+            groupBox2.Enabled = false;
+            label4.Enabled = false;
+            numericUpDown3.Enabled = false;
+            label1.Enabled = false;
+            numericUpDown4.Enabled = false;
+
+            ToolTip toolTip1 = new ToolTip();
+            toolTip1.AutoPopDelay = 5000;
+            toolTip1.InitialDelay = 100;
+            toolTip1.ReshowDelay = 500;
+            toolTip1.ShowAlways = false;
+            toolTip1.SetToolTip(button1, "The Mega Evolutions Garc is at a/1/9/3");
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();            //Open a folder browser
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                basePath = fbd.SelectedPath;
+                files = System.IO.Directory.GetFiles(fbd.SelectedPath);
+
+                if (files.Length != 826)            //Yell at the user if a file is larger or smaller than it should be
+                {
+                    MessageBox.Show("The decrypted garc must contain 826 files!");
+                    goto end;
+                }
+
+                string[] lines = Properties.Resources.mons.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);           //Code rei did that I need to keep in mind for the future
+                monNames = new List<string>(lines);         //^ Splits the mons file into a huge array based on every "return key" and new line
+
+                comboBox1.Items.AddRange(monNames.ToArray());
+                comboBox1.SelectedIndex = 0;
+            
+                checkBox1.Enabled = true;          //Re-enabled the first check box, dropdown, and save button so that the program isn't worthless
+                comboBox1.Enabled = true;
+                button2.Enabled = true;
+
+            end:
+
+                Console.Write("lol");           //I'm like fucking Macgyver
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var Evoitem1 = Convert.ToInt16(numericUpDown2.Value);           //Convert "Numericupdown" stuff to int16
+            var Evoitem2 = Convert.ToInt16(numericUpDown3.Value);
+            var Form1 = Convert.ToInt16(numericUpDown1.Value);
+            var Form2 = Convert.ToInt16(numericUpDown4.Value);
+
+            var Evoitem1b = BitConverter.GetBytes(Evoitem1);            //Convert int16 to Byte arrays
+            var Evoitem2b = BitConverter.GetBytes(Evoitem2);
+            var Form1b = BitConverter.GetBytes(Form1);
+            var Form2b = BitConverter.GetBytes(Form2);
+
+            BinaryWriter bw = new BinaryWriter(new FileStream(pathsource, FileMode.Open));          //Write Bytes to file
+
+            if (checkBox1.Checked == true && checkBox2.Checked == true)         //Set checkbox values to write
+            {
+                bw.Seek(2, 0);
+                bw.Write(1);
+
+                bw.Seek(10, 0);
+                bw.Write(1);
+            }
+
+            if (checkBox1.Checked == true && checkBox2.Checked == false)
+            {
+                bw.Seek(2, 0);
+                bw.Write(1);
+
+                bw.Seek(10, 0);
+                bw.Write(0);
+            }
+
+            if (checkBox1.Checked == false && checkBox2.Checked == false)
+            {
+                bw.Seek(2, 0);
+                bw.Write(0);
+
+                bw.Seek(10, 0);
+                bw.Write(0);
+            }
+
+            bw.Seek(4, 0);
+            bw.Write(Evoitem1b);
+
+            bw.Seek(12, 0);
+            bw.Write(Evoitem2b);
+
+            bw.Seek(0, 0);
+            bw.Write(Form1b);
+
+            bw.Seek(8, 0);
+            bw.Write(Form2b);
+
+            bw.Close();
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)           //Checkbox1 setup
+        {
+            if (checkBox1.Checked == false)
+            {
+                checkBox2.Enabled = false;
+                checkBox2.Checked = false;
+
+                groupBox1.Enabled = false;
+                label2.Enabled = false;
+                numericUpDown1.Enabled = false;
+                label3.Enabled = false;
+                numericUpDown2.Enabled = false;
+
+                groupBox2.Enabled = false;
+                label4.Enabled = false;
+                numericUpDown3.Enabled = false;
+                label1.Enabled = false;
+                numericUpDown4.Enabled = false;
+
+                numericUpDown2.Value = 0;            //Set number boxes to 0
+                numericUpDown3.Value = 0;
+                numericUpDown1.Value = 0;
+                numericUpDown4.Value = 0;
+            }
+
+            if (checkBox1.Checked == true && checkBox2.Checked == false)
+            {
+                checkBox2.Enabled = true;
+
+                groupBox2.Enabled = false;
+                label4.Enabled = false;
+                numericUpDown3.Enabled = false;
+                label1.Enabled = false;
+                numericUpDown4.Enabled = false;
+
+                groupBox1.Enabled = true;
+                label2.Enabled = true;
+                numericUpDown1.Enabled = true;
+                label3.Enabled = true;
+                numericUpDown2.Enabled = true;
+            }
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)            //Checkbox2 setup
+        {
+            if (checkBox2.Checked == false)
+            {
+                groupBox2.Enabled = false;
+                label4.Enabled = false;
+                numericUpDown3.Enabled = false;
+                label1.Enabled = false;
+                numericUpDown4.Enabled = false;
+
+                numericUpDown3.Value = 0;           //Set evolution2's checkboxes to 0
+                numericUpDown4.Value = 0;
+            }
+
+            if (checkBox2.Checked == true)
+            {
+                groupBox2.Enabled = true;
+                label4.Enabled = true;
+                numericUpDown3.Enabled = true;
+                label1.Enabled = true;
+                numericUpDown4.Enabled = true;
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var boxitem = comboBox1.SelectedItem.ToString();
+
+            pathsource = basePath + "\\" + (boxitem.Split(new string[] { " - " }, StringSplitOptions.None))[0] + ".bin";          //More rei code, but I fixed it mahself to reflect my own text file
+
+            var fi = new FileInfo(pathsource);
+
+            if (fi.Length != 24)
+            {
+                MessageBox.Show("This file is not a valid evolution table! Make sure you didn't edit it with a hex editor or anything.");
+            }
+
+            byte[] Evoitem1 = File.ReadAllBytes(pathsource).Skip(4).Take(2).ToArray();            //Get byte arrays
+            byte[] Evoitem2 = File.ReadAllBytes(pathsource).Skip(12).Take(2).ToArray();
+            byte[] Form1 = File.ReadAllBytes(pathsource).Skip(0).Take(2).ToArray();
+            byte[] Form2 = File.ReadAllBytes(pathsource).Skip(8).Take(2).ToArray();
+            byte[] Enabled1 = File.ReadAllBytes(pathsource).Skip(2).Take(2).ToArray();
+            byte[] Enabled2 = File.ReadAllBytes(pathsource).Skip(10).Take(2).ToArray();
+
+            var Evoitem1c = BitConverter.ToInt16(Evoitem1, 0);          //Convert bytes to integer
+            var Evoitem2c = BitConverter.ToInt16(Evoitem2, 0);
+            var Form1c = BitConverter.ToInt16(Form1, 0);
+            var Form2c = BitConverter.ToInt16(Form2, 0);
+            var Enabled1c = BitConverter.ToInt16(Enabled1, 0);
+            var Enabled2c = BitConverter.ToInt16(Enabled2, 0);
+
+            numericUpDown2.Value = Evoitem1c;            //Populate number boxes
+            numericUpDown3.Value = Evoitem2c;
+            numericUpDown1.Value = Form1c;
+            numericUpDown4.Value = Form2c;
+
+            if (Enabled1c == 1 && Enabled2c == 1)           //Enable or disable checkboxes depending on the byte set
+            {
+                checkBox1.Checked = true;
+                checkBox2.Checked = true;
+            }
+
+            if (Enabled1c == 1 && Enabled2c == 0)
+            {
+                checkBox1.Checked = true;
+                checkBox2.Checked = false;
+            }
+
+            if (Enabled1c == 0)
+            {
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+            }
+
+        }
+    }
+}
